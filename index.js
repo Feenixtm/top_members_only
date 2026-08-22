f17cc755b2d27cb1207fc018a3e8c6f606f7bda2
feenixtm <177155245+Feenixtm@users.noreply.github.com>
2026-08-22 00:28:02 -0700

    Added ejs and initial routes, views, and controller.js files

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
+import path from "node:path";
+import router from "./routes/allRoutes.js"
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+
+// Use ejs
+const currentDirectory = import.meta.dirname;
+app.set("views", path.join(currentDirectory, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", router);
 
 const PORT = process.env.PORT || 5051;
 
